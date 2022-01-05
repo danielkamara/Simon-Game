3b11dd0d9801c89240c045ca99de01609fb50993
Daniel <danielkamara@Daniels-MacBook-Pro-2.local>
2022-01-05 11:08:23 -0600

    added function for the computer picked colors

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-let colors = [];
+const greenButton = document.querySelector(".greenBtn");
+const redButton = document.querySelector(".redBtn");
+const yellowButton = document.querySelector(".yellowBtn");
+const blueButton = document.querySelector(".blueBtn");
 
-let randomColors = [];
+const colorButtons = [greenButton, redButton, yellowButton, blueButton];
+// let randomColors = [];
+let gamePickedColors = [];
+let userPickedColors = [];
+
+// Make a function that will pick the next colors
+const nextColor = () => {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomColor = colorButtons[randomNumber];
+  gamePickedColors.push(randomColor);
+  console.log(randomColor);
+};
